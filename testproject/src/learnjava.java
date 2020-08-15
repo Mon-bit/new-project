4ce5685deaf47e0d9f57bb14ac8f1861932dc3cc
Mon-bit <trivedimonika581@gmail.com>
2020-08-15 14:26:36 -0400

    my second code changes

--- testproject/src/learnjava.java
+++ testproject/src/learnjava.java
@@ -5,13 +5,15 @@ public class learnjava {
 	public learnjava() {
 		// TODO Auto-generated constructor stub
 	}
-	float a=500;
+
 
 	
 			
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
 
 	}
 
